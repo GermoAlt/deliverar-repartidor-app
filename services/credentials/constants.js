1d5a4ddff13329460147bc0b73e934784d864bbf
Tobías Kleppe <tkleppe@uade.edu.ar>
2022-09-16 18:40:39 -0300

    Android credentials created

--- services/credentials/constants.js
+++ services/credentials/constants.js
@@ -3,7 +3,7 @@ export default {
     EXPO_CLIENT_ID: "231782861055-gdmo7p1mosv2lfaau1igii0dgalq7rpl.apps.googleusercontent.com",
     EXPO_SECRET: "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",
     IOS_CLIENT_ID: "231782861055-qhqd08r718lpgkgt822550vrdmdri8ic.apps.googleusercontent.com",
-    ANDROID_CLIENT_ID: "",
+    ANDROID_CLIENT_ID: "231782861055-mfmops12chqdjlblpfp9qgindba4a5md.apps.googleusercontent.com",
     ASYNC_STORAGE_CREDENTIALS: "deliverarUserCredentials",
   };
   
