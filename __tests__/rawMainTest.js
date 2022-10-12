68018e1c41ea6391cca5b5ce03d650cc10e6be71
Tobías Kleppe <tkleppe@uade.edu.ar>
2022-10-11 23:26:08 -0300

    Test environment set

---
+++ __tests__/rawMainTest.js
@@ -0,0 +1,12 @@
+import React from "react";
+import renderer from "react-test-renderer";
+//import Main from "../screens/Main/Main";
+
+/*test("landing page", () => {
+    const main = renderer.create(<Main />).toJSON();
+    expect(main).toMatchSnapshot();
+});*/
+
+it("works", () => {
+    expect(1).toBe(1);
+});
